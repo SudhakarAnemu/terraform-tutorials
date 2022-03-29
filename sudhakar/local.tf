2ad16149be8ff2a43ab6afa6d5dd438a8ec7b7a5
SudhakarAnemu <sudhakar.anemu@gmail.com>
2022-03-29 06:55:32 +0530

    added files

--- sudhakar/local.tf
+++ sudhakar/local.tf
@@ -1,4 +1,11 @@
 resource "local_file" "sudhaTestFile" {
     content  = "Hey Buddy This is Sudhakar Anemu"
-    filename = "./sudhaTestFile.txt"  
+    filename = "sudhaTestFile.txt"  
+    file_permission = "0700"
+}
+
+resource "random_pet" "my-pet" {
+    prefix = "Mrs"
+    separator = "."
+    length = "1"
 }
